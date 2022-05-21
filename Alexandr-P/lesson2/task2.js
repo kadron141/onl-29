26de46825c2a262d2557c47f48d030002d04aa83
Alexandr Prokopovich <al.al.prokopovich@gmail.com>
2022-05-21 21:50:17 +0300

    adding a loop for

--- Alexandr-P/lesson2/task2.js
+++ Alexandr-P/lesson2/task2.js
@@ -1,14 +1,28 @@
-2. Написать программу “русская рулетка”
+// 2. Написать программу “русская рулетка”
 // Переменные bulletNumber (в каком гнезде пуля) и shotsFired (кол-во выстрелов) задаются в коде (оба числа не должны быть выше 7, в противном случае выводим сообщение об ошибке). На каждую попытку выстрела должно выводиться сообщение “щёлк”, на успешный выстрел “бах!”. В конце должно вывестись сообщение “игрок выжил” / “игрок не выжил”
 
 "use strict"
 
-
 let bulletNumber =  +prompt('enter variable bulletNumber');
 let shotsFired = +prompt('enter variable shotsFired');
 
-if (bulletNumber $$ shotsFired <= 7) {
-
-
+if (bulletNumber || shotsFired <= 7) {
   
+  if (bulletNumber > shotsFired) {
+
+      for (let i = 0; i < shotsFired; i++){
+        alert('щёлк');  
+      }
+        alert('игрок выжил');
+
+   } else if (bulletNumber <= shotsFired) {
+    for (let i = 0; i < bulletNumber; i++){
+       alert('щёлк'); 
+    }  
+      alert('игрок не выжил');
+     
+  }
+
+} else {
+  alert ('Вы ввели слишком большое число');
 }
