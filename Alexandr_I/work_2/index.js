9098fb24300f02f20ebd6f037e7888c1c16b12df
Sanya21i <mousec3050i@mail.ru>
2022-05-21 14:59:23 +0300

    lesson 17

---
+++ Alexandr_I/work_2/index.js
@@ -0,0 +1,24 @@
+"use strict"
+
+
+let bulletNumber = prompt('В каком гнезде пуля');
+let shotsFired = prompt('Количество выстрелов');
+
+if (bulletNumber > 8 || shotsFired > 8) {
+	console.log('Ошибка. Введите повторно значения.');
+	let bulletNumber = prompt('В каком гнезде пуля');
+	let shotsFired = prompt('Количество выстрелов');
+
+} else for (let i = 1; i <= shotsFired; i++) {
+	if (i < bulletNumber) {
+		console.log(i + '- Щёлк');
+	} else if (i = bulletNumber) {
+		console.log(i + '- Бах');
+		console.log('Игрок не выжил');
+		break;
+	}
+}
+
+if (bulletNumber > shotsFired) {
+	console.log('Игрок выжил');
+}
