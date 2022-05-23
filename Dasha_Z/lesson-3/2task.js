b8d2f1017768b445370dc375c42c9c9271e0a56e
Darya <darya.zubko98@gmail.com>
2022-05-23 14:19:30 +0300

    updated howetask. lesson 17

--- Dasha_Z/lesson-3/2task.js
+++ Dasha_Z/lesson-3/2task.js
@@ -1,25 +1,9 @@
 // Написать программу “русская рулетка”
 // Переменные bulletNumber (в каком гнезде пуля) и shotsFired (кол-во выстрелов) задаются в коде (оба числа не должны быть выше 7, в противном случае выводим сообщение об ошибке). На каждую попытку выстрела должно выводиться сообщение “щёлк”, на успешный выстрел “бах!”. В конце должно вывестись сообщение “игрок выжил” / “игрок не выжил”
+"use strict";
 
-let bulletNumber = 3;
-let shotsFired = 2;
-if (
-  bulletNumber > 7 ||
-  shotsFired > 7 ||
-  bulletNumber <= 0 ||
-  shotsFired <= 0
-) {
-  console.log("Ошибка! Числа выходят за границы");
-} else {
-  for (let i = 1; i <= shotsFired; i++) {
-    if (i == bulletNumber) {
-      console.log("бах!");
-    } else {
-      console.log("щёлк");
-    }
-  }
-}
-
+let bulletNumber = 9;
+let shotsFired = 6;
 if (
   bulletNumber > 7 ||
   shotsFired > 7 ||
@@ -27,8 +11,17 @@ if (
   shotsFired <= 0
 ) {
   alert("Ошибка! Числа выходят за границы");
-} else if (shotsFired < bulletNumber) {
-  alert("Вы выжили!");
 } else {
-  alert("Вы не выжили");
+  for (let i = 1; i <= shotsFired; i++) {
+    if (i == bulletNumber) {
+      console.log("бах!");
+      alert("Вы не выжили");
+      break;
+    } else {
+      console.log("щёлк");
+    }
+  }
+  if (bulletNumber > shotsFired) {
+    alert("Вы выжили");
+  }
 }
